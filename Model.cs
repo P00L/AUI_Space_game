9574342f2ad9a2f19320994ec965af04b9c4a0d9
Dario <daaar.mapelli@gmail.com>
2015-12-04 16:38:35 +0100

    commit

---
+++ Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuiSpaceGame.Model
+{
+    class Class1
+    {
+        public int ciao()
+        {
+            return 1;
+        }
+    }
+    
+    class Class2
+    {
+
+    }
+}
